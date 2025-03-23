a04f4bc3d9f4265119986a6d0c2c9adbaa6c15dc
nami <minami.nami08121519@icloud.com>
2025-03-24 04:56:23 +0900

    debug

--- denops/popup_preview/documentation.ts
+++ denops/popup_preview/documentation.ts
@@ -111,7 +111,7 @@ export class DocHandler {
       this.showFloating(denops, cache.lines, config);
       return;
     }
-    console.log(`decoded: ${item}`);
+    console.log(`item: ${item}`);
     const maybe = await searchUserdata(denops, item, config, info.selected);
     if (!maybe.found) {
       this.closeWin(denops);
