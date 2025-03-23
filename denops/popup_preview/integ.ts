11f65aa26afd95a8537dda5032e1246132438b02
nami <minami.nami08121519@icloud.com>
2025-03-24 04:41:38 +0900

    debug

--- denops/popup_preview/integ.ts
+++ denops/popup_preview/integ.ts
@@ -123,7 +123,7 @@ export async function searchUserdata(
   }
 
   // vim-lsp
-  denops.call("echomsg", `decoded user_data: ${decoded}`);
+  denops.call("echo", `decoded user_data: ${decoded}`);
   if ("vim-lsp/key" in decoded) {
     const lspitem = await denops.call(
       "lsp#omni#get_managed_user_data_from_completed_item",
